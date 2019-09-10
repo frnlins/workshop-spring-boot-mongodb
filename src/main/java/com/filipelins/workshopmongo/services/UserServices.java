63439c8102560d40e3ca81eccb088144f2249d5e
filipe.lins <filipe.lins@dataprev.gov.br>
2019-09-10 15:36:26 -0300

    inserção de usuário via post

--- src/main/java/com/filipelins/workshopmongo/services/UserServices.java
+++ src/main/java/com/filipelins/workshopmongo/services/UserServices.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.filipelins.workshopmongo.domain.User;
+import com.filipelins.workshopmongo.dto.UserDTO;
 import com.filipelins.workshopmongo.repository.UserRepository;
 import com.filipelins.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserServices {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Usuário não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repo.save(user);
+	}
+	
+	public User fromDTO(UserDTO userDTO) {
+		return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
 	}
 }
