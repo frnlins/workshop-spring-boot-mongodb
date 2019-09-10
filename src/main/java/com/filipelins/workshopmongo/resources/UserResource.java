694ca57d6721872ad02624644252085dc78ffb40
filipe.lins <filipe.lins@dataprev.gov.br>
2019-09-10 11:47:49 -0300

    entity user and rest call

---
+++ src/main/java/com/filipelins/workshopmongo/resources/UserResource.java
@@ -0,0 +1,29 @@
+package com.filipelins.workshopmongo.resources;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.filipelins.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+	@GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = new ArrayList<>();
+
+		User maria = new User("1", "Maria Brown", "maria.brown@gmail.com");
+		User alex = new User("2", "Alex Green", "alex.green@gmail.com");
+
+		list.addAll(Arrays.asList(maria, alex));
+
+		return ResponseEntity.ok(list);
+	}
+}
